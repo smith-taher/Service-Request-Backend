cc5bb7ab1abc284b75d9a162a5e10d2fc74a3397
Joel Smith <joelsmith@Joels-MacBook-Pro.local>
2018-05-03 09:22:13 -0400

    added json package and express on server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
-const http = require('http');
-const pg = require('pg-promise')();
-const db = pg(process.env.DB_PATH);
-const jwt = require('jsonwebtoken');
-const fs = require('fs');
-
-//listen on 3000
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+res.send('Hey Builders!');
+})
+app.listen(3000, () => {
+console.log('Running on port 3000');
+})
